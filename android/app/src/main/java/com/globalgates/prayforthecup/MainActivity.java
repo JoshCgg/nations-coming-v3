45b69fb3b862cc9b8c0f904e4da3e1be7de4d0ff
Josh Collins <jcollins@globalgates.info>
2026-05-26 20:22:00 -0400

    fix: add ModifiedMainActivityForSocialLoginPlugin to MainActivity for @capgo/capacitor-social-login Android support

--- android/app/src/main/java/com/globalgates/prayforthecup/MainActivity.java
+++ android/app/src/main/java/com/globalgates/prayforthecup/MainActivity.java
@@ -1,5 +1,8 @@
 package com.globalgates.prayforthecup;
 
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {}
